647a8d8acb9b168f9efd8570ac5004367acb43d8
Renja <renja.grotemeyer@gmail.com>
2023-09-22 14:01:13 +0200

    docker test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {port: 4000},
+	preview: {port: 3000},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
