0aeb88410a97ca8cd8d7afa711053f2ddea68934
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2023-10-10 21:23:54 +0200

    everything just better (ranked ENUMs)

--- backend/api/classes/rankedentries.py
+++ backend/api/classes/rankedentries.py
@@ -1,11 +1,33 @@
 from pydantic import BaseModel
 from typing import Optional
 
+from enum import Enum
+
+
+class Tiers(str, Enum):
+    IRON = "IRON"
+    BRONZE = "BRONZE"
+    SILVER = "SILVER"
+    GOLD = "GOLD"
+    PLATINUM = "PLATINUM"
+    EMERALD = "EMERALD"
+    DIAMOND = "DIAMOND"
+    MASTER = "MASTER"
+    GRANDMASTER = "GRANDMASTER"
+    CHALLENGER = "CHALLENGER"
+
+
+class Ranks(str, Enum):
+    I = "I"
+    II = "II"
+    III = "III"
+    IV = "IV"
+
 
 class RankedEntry(BaseModel):
     leagueId: str
-    tier: str
-    rank: str
+    tier: Tiers
+    rank: Ranks
     lp: int
     wins: int
     losses: int
@@ -14,7 +36,23 @@ class RankedEntry(BaseModel):
     freshBlood: bool
     hotStreak: bool
 
+    def __init__(self, **args):
+        args["lp"] = args["leaguePoints"]
+
+        super().__init__(**args)
+
 
 class RankedEntries(BaseModel):
     solo: Optional[RankedEntry]
     flex: Optional[RankedEntry]
+
+    def __init__(self, **args):
+        if "440" in args:
+            args["flex"] = args["440"]
+            del args["440"]
+
+        if "420" in args:
+            args["solo"] = args["420"]
+            del args["420"]
+
+        super().__init__(**args)
--- backend/api/classes/summoner.py
+++ backend/api/classes/summoner.py
@@ -18,15 +18,4 @@ class Summoner(BaseModel):
         args["entries"] = args["league_entries"]
         args["region"] = args["platform"]
 
-        if "440" in args["entries"]:
-            args["entries"]["flex"] = args["entries"]["440"]
-            del args["entries"]["440"]
-
-        if "420" in args["entries"]:
-            args["entries"]["solo"] = args["entries"]["420"]
-            del args["entries"]["420"]
-
-        for league in args["entries"]:
-            args["entries"][league]["lp"] = args["entries"][league]["leaguePoints"]
-
         super().__init__(**args)
